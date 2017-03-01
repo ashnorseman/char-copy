3746e0583dc754944bf33a273b0eb5370e869a4f
ZHANG, Ash <ash.zhang@philips.com>
2017-03-01 14:46:16 +0800

    glyph-maker

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@
 import './style.scss';
 
 import * as Clipboard from 'clipboard';
+import { GlyphMaker } from './GlyphMaker';
 
-const clipboard = new Clipboard(document.querySelectorAll('li'), {
+
+const clipboard = new Clipboard(document.querySelectorAll('.glyph'), {
   text: (trigger) => {
     return trigger.innerText;
   }
@@ -19,3 +21,19 @@ clipboard.on('success', (e) => {
     e.trigger.classList.remove('copy-success');
   }, 300);
 });
+
+const glyphMaker: GlyphMaker = new GlyphMaker(<HTMLCanvasElement>document.getElementById('glyph-canvas'));
+
+glyphMaker.on('change:idc', (idc: string) => {
+  document.getElementById('idc').innerText = idc;
+});
+
+const button = document.getElementById('draw-button');
+
+button.addEventListener('click', () => {
+  glyphMaker.drawText(
+    (<HTMLInputElement>document.getElementById('block-1')).value,
+    (<HTMLInputElement>document.getElementById('block-2')).value,
+    (<HTMLInputElement>document.getElementById('block-3')).value
+  );
+}, false);
